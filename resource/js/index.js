4417b19ef1faa9ef07c72b3d6e5081dbdbdd2bfe
minSW <minsw08@gmail.com>
2019-06-08 19:09:50 +0900

    Add buttons to set default and copy the code

--- resource/js/index.js
+++ resource/js/index.js
@@ -1,25 +1,24 @@
 $(function() {
-  $(".form-control").change( function() {
-       updateCode();
-       updateView();
+  $(".form-control").change(function() {
+    updateCode();
+    updateView();
   });
 });
 
 function updateCode() {
-  $('.code').text("export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'"
-                  +"\n--color=fg:" + getColorText($('#fg')) + ",bg:" + getColorText($('#bg')) + ",hl:" + $('#hl').val()
-                  +",fg+:" + $('#fgp').val() + ",bg+:" + $('#bgp').val() + ",hl+:" + $('#hlp').val()
-                  +"\n--color=info:" + $('#info').val() + ",prompt:" + $('#prompt').val() + ",pointer:" + $('#pointer').val()
-                  +",marker:" + $('#marker').val() + ",spinner:" + $('#spinner').val() + ",header:" + $('#header').val()
-                  +"\n'");
-  $('.code').html($('.code').html().replace(/\n/g,'<br/>'));
-  
+  $('.code').text("export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'" +
+    "\n --color=fg:" + getColorText($('#fg')) + ",bg:" + getColorText($('#bg')) + ",hl:" + $('#hl').val() +
+    "\n --color=fg+:" + $('#fgp').val() + ",bg+:" + $('#bgp').val() + ",hl+:" + $('#hlp').val() +
+    "\n --color=info:" + $('#info').val() + ",prompt:" + $('#prompt').val() + ",pointer:" + $('#pointer').val() +
+    "\n --color=marker:" + $('#marker').val() + ",spinner:" + $('#spinner').val() + ",header:" + $('#header').val() +
+    "'");
+  $('.code').html($('.code').html().replace(/\n/g, '<br/>'));
+
 };
 
 function updateView() {
   updateColorView($(".fg"), "color", $('#fg').val());
   updateColorView($(".bg"), "background-color", $('#bg').val());
-
   $(".fgp").css("color", $('#fgp').val());
   $(".bgp").css("background-color", $('#bgp').val());
   $(".hl").css("color", $('#hl').val());
@@ -43,4 +42,30 @@ function updateColorView(ele, styleType, val) {
   if (val) {
     ele.css(styleType, val);
   }
+}
+
+function setDefault(ele) {
+  $(ele).val("")
+  updateCode();
+}
+
+function copyToClipboard() {
+  var $temp = $("<input>");
+  $("body").append($temp);
+  $temp.val($("#export-code").text()).select();
+  document.execCommand("copy");
+  $temp.remove();
+
+  $("#copy-btn").popover('show');
+  setTimeout(function() {
+    $("#copy-btn").popover('destroy');
+  }, 1000);
+}
+
+function applyToFzf() {
+  //TODO : implement apply
+  $("#apply-btn").popover('show');
+  setTimeout(function() {
+    $("#apply-btn").popover('destroy');
+  }, 1000);
 }
